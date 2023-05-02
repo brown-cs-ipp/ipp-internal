b2c6fe0ce7fbf3d151b002d82df6965393d08e84
jbarlas <justin_barlas@brown.edu>
2023-05-02 00:12:32 -0400

    Updates

---
+++ precache-manifest.cc8264d03e9ff60ad13a4db887079043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "762e8ab3176ff2ed3333",
+    "url": "/static/css/main.a5a9227f.chunk.css"
+  },
+  {
+    "revision": "762e8ab3176ff2ed3333",
+    "url": "/static/js/main.8551ae7b.chunk.js"
+  },
+  {
+    "revision": "c81066000cd0dffd9c96",
+    "url": "/static/js/runtime~main.447739d8.js"
+  },
+  {
+    "revision": "c66c81dd1c3229550789",
+    "url": "/static/js/2.7c206189.chunk.js"
+  },
+  {
+    "revision": "03401dfc1b6d28949ba8",
+    "url": "/static/js/3.779d2b58.chunk.js"
+  },
+  {
+    "revision": "4991330da3ebe99c6c96ff062cdffdf9",
+    "url": "/index.html"
+  }
+];
